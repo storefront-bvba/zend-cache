53e7955013a08321edca4001916846b959842f0d
Wouter Samaey <wouter.samaey@storefront.be>
2025-12-01 10:45:30 +0100

    Types

--- library/Zend/Cache/Core.php
+++ library/Zend/Cache/Core.php
@@ -419,25 +419,7 @@ class Zend_Cache_Core
         return $this->_backend->remove($id);
     }
 
-    /**
-     * Clean cache entries
-     *
-     * Available modes are :
-     * 'all' (default)  => remove all cache entries ($tags is not used)
-     * 'old'            => remove too old cache entries ($tags is not used)
-     * 'matchingTag'    => remove cache entries matching all given tags
-     *                     ($tags can be an array of strings or a single string)
-     * 'notMatchingTag' => remove cache entries not matching one of the given tags
-     *                     ($tags can be an array of strings or a single string)
-     * 'matchingAnyTag' => remove cache entries matching any given tags
-     *                     ($tags can be an array of strings or a single string)
-     *
-     * @param string $mode
-     * @param array|string $tags
-     * @return boolean True if ok
-     * @throws Zend_Cache_Exception
-     */
-    public function clean($mode = 'all', $tags = [])
+    public function clean(string $mode = 'all', array $tags = []): bool
     {
         if (!$this->_options['caching']) {
             return true;
